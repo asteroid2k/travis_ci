bcae38ae429b00a1ff43fd578932f099dd41e15d
Kwaku Okyere Adusei <dobridobrev900@gmail.com>
2023-02-18 08:20:58 +0000

    add env variables

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+const SLEEP_TIME = process.env.SLEEP_TIME || 5000;
+const CONTAINER = process.env.CONTAINER || "";
+
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
+  while (true) {
+    console.log(`${CONTAINER} containers rule!`);
+    await sleep(SLEEP_TIME);
   }
 }
 
